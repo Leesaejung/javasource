265cc1b25d5d765f422eddac3d8b6a8edf2f1615
Saejung <leeesaejung@gmail.com>
2023-03-27 17:57:51 +0900

    empMAIN, empDAO 작업 완료

--- database/src/database2/EmpDAO.java
+++ database/src/database2/EmpDAO.java
@@ -146,6 +146,61 @@ public void close(Connection con, PreparedStatement pstmt) {
 			close(con, pstmt);
 		}
 		return status;
+	}//급여 수정 완료
+	
+	//사원 삭제
+	public boolean remove(int empno) {
+		boolean status = false;
+		
+		try {
+			con = getConnection();
+			
+			//empno 일치 시 사원 삭제
+			String sql = "delete from emp_temp where empno = ? ";
+			pstmt = con.prepareStatement(sql);
+			// ? 처리
+			pstmt.setInt(1, empno);
+			
+			int result = pstmt.executeUpdate();
+			
+			if (result>0) status = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(con, pstmt);
+		}
+		return status;
+	}
+	
+	// 새로운 사원 추가
+	public boolean insert(EmpDTO empDTO) {
+		
+		boolean status = false;
+		
+		try {
+			con = getConnection();
+			String sql = "insert into emp_temp(empno, ename, job, mgr, hiredate, sal,comm,deptno) ";
+			sql +="values(?,?,?,?,sysdate,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, empDTO.getEmpno());
+			pstmt.setString(2, empDTO.getEname());
+			pstmt.setString(3, empDTO.getJob());
+			pstmt.setInt(4, empDTO.getMgr());
+			pstmt.setInt(5, empDTO.getSal());
+			pstmt.setInt(6, empDTO.getComm());
+			pstmt.setInt(7, empDTO.getDeptno());
+			
+			
+			int result = pstmt.executeUpdate();
+			if(result>0) status = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(con, pstmt);
+		}
+		return status;
 	}
 	
 }
